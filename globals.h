d8c97325ad153bb6eb3a3354014dfe307ab7a238
misha <misha@062fa24c-051e-974a-901b-19274578d6b4>
2009-02-26 19:13:37 +0000

    tools moved from globals to tools module

--- globals.h
+++ globals.h
@@ -9,6 +9,7 @@
 #define GLOBALS_H_
 
 
+#include "console.h"
 #include "exmath.h"
 #include "resource.h"
 #include <windows.h> // for HDC
@@ -302,137 +303,6 @@ private:
 	bool m_panning;
 	int m_prevX;
 	int m_prevY;
-};
-
-
-class DrawLinesTool : public Tool
-{
-public:
-	DrawLinesTool() {}
-	virtual bool ProcessInput(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
-	virtual void Start();
-	virtual void Command(const std::wstring & cmd);
-	virtual void Exiting();
-private:
-	std::list<Point<double> > m_points;
-	std::auto_ptr<CadLine> m_fantomLine;
-	void RecalcFantomsHandler();
-	void FeedPoint(const Point<double> & pt);
-	void UpdatePrompt();
-};
-
-
-class DrawPLineTool : public Tool
-{
-public:
-	DrawPLineTool() : m_arcDir(1, 0) {}
-	virtual void Start();
-	virtual bool ProcessInput(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
-	virtual void Command(const std::wstring & cmd);
-	virtual void Exiting();
-private:
-	enum State
-	{
-		StateSelFirstPt,
-		StateSelLineSecondPt,
-		StateSelArcEndPt,
-		StateSelArcDirection,
-	};
-	State m_state;
-	std::auto_ptr<CadLine> m_fantomLine;
-	std::auto_ptr<CadArc> m_fantomArc;
-	CadPolyline * m_result;
-	Point<double> m_arcDir;
-	void SetPrompt();
-	void RecalcFantomsHandler();
-	void FeedFirstPoint(const Point<double> & pt);
-	void FeedLineSecondPoint(const Point<double> & pt);
-	void FeedArcEndPoint(const Point<double> & pt);
-	void FeedArcDirection(const Point<double> & endpt);
-};
-
-
-class DrawCircleTool : public Tool
-{
-public:
-	virtual bool ProcessInput(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
-	virtual void Start();
-	virtual void Command(const std::wstring & cmd);
-private:
-	enum State
-	{
-		StateSelectingCenter,
-		StateSelectingRadius,
-		StateSelectingDiameter,
-		State2PtSelectingFirstPoint,
-		State2PtSelectingSecondPoint,
-		State3PtSelectingFirstPoint,
-		State3PtSelectingSecondPoint,
-		State3PtSelectingThirdPoint,
-	};
-	State m_state;
-	std::auto_ptr<CadCircle> m_cadCircle;
-	std::auto_ptr<CadLine> m_line;
-	Point<double> m_firstPoint, m_secondPoint;
-	void RecalcFantomsHandler();
-	void FeedCenter(const Point<double> & center);
-	void FeedRadius(double radius);
-	void Feed2PtFirstPoint(const Point<double> & pt);
-	void Feed2PtSecondPoint(const Point<double> & pt);
-	void Feed3PtFirstPoint(const Point<double> & pt);
-	void Feed3PtSecondPoint(const Point<double> & pt);
-	void Feed3PtThirdPoint(const Point<double> & pt);
-	void CalcCircleFrom3Pt(const Point<double> & thirdPoint);
-};
-
-
-class DrawArcsTool : public Tool
-{
-public:
-	virtual bool ProcessInput(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
-	virtual void Start();
-	virtual void Command(const std::wstring & cmd);
-private:
-	enum State
-	{
-		StateSelectingFirstPoint,
-		StateSelectingSecondPoint,
-		StateSelectingThirdPoint
-	};
-	State m_state;
-	std::auto_ptr<CadLine> m_fantomLine;
-	Point<double> m_secondPoint;
-	std::auto_ptr<CadArc> m_fantomArc;
-	void RecalcFantomsHandler();
-	void FeedFirstPoint(const Point<double> & pt);
-	void FeedSecondPoint(const Point<double> & pt);
-	void FeedThirdPoint(const Point<double> & pt);
-	void CalcArcFrom3Pt(const Point<double> & thirdPoint);
-};
-
-
-class MoveTool : public Tool
-{
-public:
-	virtual bool ProcessInput(HWND hwnd, unsigned int msg, WPARAM wparam, LPARAM lparam);
-	virtual void Start();
-	virtual void Command(const std::wstring & cmd);
-	virtual void Exiting();
-private:
-	enum State
-	{
-		StateChoosingBasePoint,
-		StateChoosingDestPoint,
-	};
-	State m_state;
-	Point<double> m_basePoint;
-	std::vector<CadObject *> m_objects;
-	std::auto_ptr<CadLine> m_fantomLine;
-	void DeleteCopies();
-	void RecalcFantomsHandler();
-	void CalcPositions(const Point<double> & pt);
-	void FeedBasePoint(const Point<double> & pt);
-	void FeedDestPoint(const Point<double> & pt);
 };
 
 
@@ -832,6 +702,21 @@ inline std::wstring IntToWstr(int value)
 	int len = std::swprintf(buffer, L"%d", value);
 	assert(len > 0);
 	return std::wstring(buffer, len);
+}
+
+
+inline bool TryParsePoint2D(const std::wstring & str, Point<double> & point)
+{
+	return (std::swscanf(str.c_str(), L"%lf,%lf", &point.X, &point.Y) == 2);
+}
+
+
+inline bool ParsePoint2D(const std::wstring & str, Point<double> & point)
+{
+	if (TryParsePoint2D(str, point))
+		return true;
+	g_console.Log(L"Invalid 2D point");
+	return false;
 }
 
 #endif /* GLOBALS_H_ */
